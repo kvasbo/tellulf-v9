7ae05d1c54c9eb5b8ac956dc58788ca3abad1418
Audun Kvasbø <audun@kvasbo.no>
2024-12-15 16:27:17 +0100

    Squashed commit of the following:
    commit 08ebabb10d9f013bc40ca275640c441ed1cf2479
    Author: Audun Kvasbø <audun@kvasbo.no>
    Date:   Sun Dec 15 16:27:03 2024 +0100
    
        New nowcast
    
    commit cf85d358fefece884b6190f3776fc04728daf06c
    Author: Audun Kvasbø <audun@kvasbo.no>
    Date:   Sun Dec 15 15:58:15 2024 +0100
    
        WIP

--- src/components/HourlyForecast/HourlyForecast.svelte
+++ src/components/HourlyForecast/HourlyForecast.svelte
@@ -1,22 +1,47 @@
 <script lang="ts">
 	import { hourlyForecastStore } from '$lib/client/store';
+
+	$: hourlyForecastStore;
+
+	function calculateMinMax() {
+		let min = 100;
+		let max = -100;
+		$hourlyForecastStore.forEach((forecast) => {
+			if (forecast.instant.air_temperature < min) {
+				min = forecast.instant.air_temperature;
+			}
+			if (forecast.instant.air_temperature > max) {
+				max = forecast.instant.air_temperature;
+			}
+		});
+		// Round to nearest five up and down
+		min = Math.floor(min / 5) * 5;
+		max = Math.ceil(max / 5) * 5;
+
+		// Function to map a value from one range to another
+		const mapToRange = (value) => {
+			return ((value - min) / (max - min)) * 80;
+		};
+
+		return {
+			min,
+			max,
+			mapToRange
+		};
+	}
+	const { min, max, mapToRange } = calculateMinMax();
 </script>
 
 <nowcast>
 	<div class="weather">
 		{#each $hourlyForecastStore.slice(1, 19) as forecast}
 			<forecast>
-				<div class="time">
-					{forecast.hour}
-				</div>
-				<div class="weather_symbol">
+				<span class="forecastMovablePart" style="margin-bottom: {20 + (mapToRange(forecast.instant.air_temperature) * 10)}%;">
 					<img class="weather_icon" alt="symbol" src="weathericon/png/{forecast.symbol}.png" />
-				</div>
-				<div class="temperature">
-					{forecast.instant.air_temperature}°
-				</div>
-				<div class="forecast_prob">
-					<div class="forecast_prob">
+					<div class="temperature">
+						{forecast.instant.air_temperature}°
+					</div>
+					<div class="forecastProb">
 						{#if forecast.details.probability_of_precipitation != 0}
 							{forecast.details.probability_of_precipitation}%
 							<br />
@@ -26,6 +51,9 @@
 							&nbsp;
 						{/if}
 					</div>
+				</span>
+				<div class="time">
+					{forecast.hour}
 				</div>
 			</forecast>
 		{/each}
