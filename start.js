d3317649bb9fc6e318fe8f7582226bea814f765e
Audun Kvasbø <audun@kvasbo.no>
2025-08-02 10:45:42 +0200

    Update start.js

--- start.js
+++ start.js
@@ -1,2 +1,8 @@
-import 'dotenv/config';
+import { config } from 'dotenv';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+config({ path: join(__dirname, '.env') });
+
 import './build/index.js';
