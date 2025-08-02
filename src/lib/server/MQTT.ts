f350714be3171e6f45500c64a0c8a38b3d45f41f
Audun Kvasbø <audun@kvasbo.no>
2025-08-02 14:40:06 +0200

    Env fix

--- src/lib/server/MQTT.ts
+++ src/lib/server/MQTT.ts
@@ -2,8 +2,8 @@ import * as mqtt from 'mqtt';
 import { env } from '$env/dynamic/private';
 
 const options = {
-	username: env.MQTT_USER,
-	password: env.MQTT_PASS,
+	username: process.env.MQTT_USER || env.MQTT_USER,
+	password: process.env.MQTT_PASS || env.MQTT_PASS,
 	clientId: 'tellulf-' + Math.random().toString(16).substring(2, 8),
 	keepalive: 15
 };
@@ -22,11 +22,12 @@ export class MqttClient {
 
 	// Connect to MQTT broker
 	private constructor() {
-		console.log('Connecting to MQTT host:', env.MQTT_HOST);
-		this.client = mqtt.connect(env.MQTT_HOST, options);
+		const mqttHost = process.env.MQTT_HOST || env.MQTT_HOST;
+		console.log('Connecting to MQTT host:', mqttHost);
+		this.client = mqtt.connect(mqttHost, options);
 		this.client
 			.on('connect', () => {
-				this.log(`${options.clientId} connected to ${env.MQTT_HOST}`);
+				this.log(`${options.clientId} connected to ${mqttHost}`);
 				this.client.subscribe('#');
 				this.client.publish('tellulf/poll', 'Tellulf is online and polling');
 			})
@@ -34,14 +35,13 @@ export class MqttClient {
 				this.log('Disconnected');
 			})
 			.on('error', (error) => {
-				console.log('MQTT Error:', error.message || error);
-				console.log('Full error:', error);
+				console.log('MQTT error:', mqttHost, error);
 				this.client.end();
-				console.log('MQTT client ended, reconnecting in 2 seconds');
+				console.log('MQTT client errored, reconnecting in 2 seconds');
 				setTimeout(() => this.client.reconnect(), 2000);
 			})
 			.on('reconnect', () => {
-				console.log('MQTT reconnecting');
+				console.log(`MQTT reconnecting to ${mqttHost}`);
 			});
 	}
 
