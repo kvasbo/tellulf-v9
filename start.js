64d371c6dbd6b78c74efc418040c034ee20bb262
Audun Kvasbø <audun@kvasbo.no>
2025-08-02 11:06:05 +0200

    Fix pm2

--- start.js
+++
@@ -1,8 +0,0 @@
-import { config } from 'dotenv';
-import { dirname, join } from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-config({ path: join(__dirname, '.env') });
-
-import './build/index.js';
