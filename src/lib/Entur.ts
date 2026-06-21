5020f05c44230a828541f46900511f8a320cd01c
Audun Kvasbø <audun@kvasbo.no>
2026-06-21 16:59:33 +0200

    Biome check

--- src/lib/Entur.ts
+++ src/lib/Entur.ts
@@ -1,123 +1,123 @@
-import { XMLParser } from "fast-xml-parser";
-import { z } from "zod";
+import { XMLParser } from 'fast-xml-parser';
+import { z } from 'zod';
 
 const enturSchema = z.object({
-  Siri: z.object({
-    ServiceDelivery: z.object({
-      EstimatedTimetableDelivery: z.object({
-        EstimatedJourneyVersionFrame: z.object({
-          EstimatedVehicleJourney: z.array(
-            z.object({
-              EstimatedCalls: z
-                .object({
-                  EstimatedCall: z.union([
-                    z.array(
-                      z.object({
-                        StopPointName: z.string().optional(),
-                        ExpectedDepartureTime: z.string().optional(),
-                        DestinationDisplay: z.string(),
-                      }),
-                    ),
-                    z.object({
-                      StopPointName: z.string().optional(),
-                      ExpectedDepartureTime: z.string().optional(),
-                      DestinationDisplay: z.string(),
-                    }),
-                  ]),
-                })
-                .optional(),
-              LineRef: z.string(),
-              DirectionRef: z.number(),
-            }),
-          ),
-        }),
-      }),
-    }),
-  }),
+	Siri: z.object({
+		ServiceDelivery: z.object({
+			EstimatedTimetableDelivery: z.object({
+				EstimatedJourneyVersionFrame: z.object({
+					EstimatedVehicleJourney: z.array(
+						z.object({
+							EstimatedCalls: z
+								.object({
+									EstimatedCall: z.union([
+										z.array(
+											z.object({
+												StopPointName: z.string().optional(),
+												ExpectedDepartureTime: z.string().optional(),
+												DestinationDisplay: z.string(),
+											}),
+										),
+										z.object({
+											StopPointName: z.string().optional(),
+											ExpectedDepartureTime: z.string().optional(),
+											DestinationDisplay: z.string(),
+										}),
+									]),
+								})
+								.optional(),
+							LineRef: z.string(),
+							DirectionRef: z.number(),
+						}),
+					),
+				}),
+			}),
+		}),
+	}),
 });
 
 interface Train {
-  time: string;
-  destination: string;
+	time: string;
+	destination: string;
 }
 
 export class Entur {
-  trains: Train[] = [];
+	trains: Train[] = [];
 
-  constructor() {
-    setTimeout(() => this.Update(), 1000);
-    setInterval(() => {
-      this.Update();
-    }, 60000);
-  }
+	constructor() {
+		setTimeout(() => this.Update(), 1000);
+		setInterval(() => {
+			this.Update();
+		}, 60000);
+	}
 
-  getTrains(): Train[] {
-    return this.trains;
-  }
+	getTrains(): Train[] {
+		return this.trains;
+	}
 
-  async Update() {
-    try {
-      const parser = new XMLParser();
-      const xmlData = await fetch(
-        "https://api.entur.io/realtime/v1/rest/et?datasetId=RUT",
-      );
-      const text = await xmlData.text();
-      const parsed = parser.parse(text);
-      const valid = enturSchema.safeParse(parsed);
-      if (!valid.success) {
-        console.error("Invalid data from Entur; keeping previous departures");
-        return;
-      }
-      const trips =
-        valid.data.Siri.ServiceDelivery.EstimatedTimetableDelivery
-          .EstimatedJourneyVersionFrame.EstimatedVehicleJourney;
-      const filteredTrips = trips.filter((trip) => {
-        // Check if the trip has any EstimatedCalls
-        if (
-          !trip.EstimatedCalls?.EstimatedCall ||
-          !Array.isArray(trip.EstimatedCalls.EstimatedCall) ||
-          trip.EstimatedCalls.EstimatedCall.length === 0
-        ) {
-          return false;
-        }
-        // Check if the trip is on the correct line and direction
-        if (trip.LineRef !== "RUT:Line:1" || trip.DirectionRef !== 1) {
-          return false;
-        }
-        if (
-          !Array.isArray(trip.EstimatedCalls.EstimatedCall) ||
-          trip.EstimatedCalls.EstimatedCall.filter(
-            (call) => call.StopPointName === "Slemdal",
-          ).length === 0
-        ) {
-          return false;
-        }
-        return true;
-      });
-      const filteredTrains = filteredTrips.map(
-        (trip): { time: string; destination: string } => {
-          // @ts-expect-error - We have already checked that this is an array
-          const found = trip.EstimatedCalls?.EstimatedCall.find(
-            (stop: { StopPointName: string }) =>
-              stop.StopPointName === "Slemdal",
-          );
-          return {
-            time: found.ExpectedDepartureTime,
-            destination: found.DestinationDisplay,
-          };
-        },
-      );
+	async Update() {
+		try {
+			const parser = new XMLParser();
+			const xmlData = await fetch(
+				'https://api.entur.io/realtime/v1/rest/et?datasetId=RUT',
+			);
+			const text = await xmlData.text();
+			const parsed = parser.parse(text);
+			const valid = enturSchema.safeParse(parsed);
+			if (!valid.success) {
+				console.error('Invalid data from Entur; keeping previous departures');
+				return;
+			}
+			const trips =
+				valid.data.Siri.ServiceDelivery.EstimatedTimetableDelivery
+					.EstimatedJourneyVersionFrame.EstimatedVehicleJourney;
+			const filteredTrips = trips.filter((trip) => {
+				// Check if the trip has any EstimatedCalls
+				if (
+					!trip.EstimatedCalls?.EstimatedCall ||
+					!Array.isArray(trip.EstimatedCalls.EstimatedCall) ||
+					trip.EstimatedCalls.EstimatedCall.length === 0
+				) {
+					return false;
+				}
+				// Check if the trip is on the correct line and direction
+				if (trip.LineRef !== 'RUT:Line:1' || trip.DirectionRef !== 1) {
+					return false;
+				}
+				if (
+					!Array.isArray(trip.EstimatedCalls.EstimatedCall) ||
+					trip.EstimatedCalls.EstimatedCall.filter(
+						(call) => call.StopPointName === 'Slemdal',
+					).length === 0
+				) {
+					return false;
+				}
+				return true;
+			});
+			const filteredTrains = filteredTrips.map(
+				(trip): { time: string; destination: string } => {
+					// @ts-expect-error - We have already checked that this is an array
+					const found = trip.EstimatedCalls?.EstimatedCall.find(
+						(stop: { StopPointName: string }) =>
+							stop.StopPointName === 'Slemdal',
+					);
+					return {
+						time: found.ExpectedDepartureTime,
+						destination: found.DestinationDisplay,
+					};
+				},
+			);
 
-      this.trains = filteredTrains.sort((a, b) => {
-        return new Date(a.time).getTime() - new Date(b.time).getTime();
-      });
+			this.trains = filteredTrains.sort((a, b) => {
+				return new Date(a.time).getTime() - new Date(b.time).getTime();
+			});
 
-      console.log(`Entur updated with ${this.trains.length} trains`);
-    } catch (error) {
-      console.error(
-        "Error updating Entur; keeping previous departures:",
-        error,
-      );
-    }
-  }
+			console.log(`Entur updated with ${this.trains.length} trains`);
+		} catch (error) {
+			console.error(
+				'Error updating Entur; keeping previous departures:',
+				error,
+			);
+		}
+	}
 }
