b56db884c9b8c3a4e721687a8ec27a796b6eed15
Audun Kvasbø <audun@kvasbo.no>
2026-06-26 16:14:29 +0200

    Remove some noise

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # tellulf-v9
 
 Personal project. Nothing to see here, move along.
-
-On the client, kill the kiosk with "pkill firefox"
--- client-config/kiosk.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export DISPLAY=:0
-echo "Launching Firefox" >> /home/kvasbo/kiosk.log
-/usr/bin/firefox --fullscreen 'http://192.168.1.25:3000' &
